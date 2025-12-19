58dcbf53ef4463dda2edaf4a760dd1fa8633f607
ZionDere2 <165028736+ZionDere2@users.noreply.github.com>
2025-12-19 16:06:16 +0100

    Add files via upload

---
+++ webserver_tcp_udp-main/cmake-build-debug/CMakeFiles/webserver.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/dymas/Desktop/Uni/RN_VS/RN-Praxis/Praxis 2 Norm/praxis2/data.c" "CMakeFiles/webserver.dir/data.c.o" "gcc" "CMakeFiles/webserver.dir/data.c.o.d"
+  "/mnt/c/Users/dymas/Desktop/Uni/RN_VS/RN-Praxis/Praxis 2 Norm/praxis2/http.c" "CMakeFiles/webserver.dir/http.c.o" "gcc" "CMakeFiles/webserver.dir/http.c.o.d"
+  "/mnt/c/Users/dymas/Desktop/Uni/RN_VS/RN-Praxis/Praxis 2 Norm/praxis2/util.c" "CMakeFiles/webserver.dir/util.c.o" "gcc" "CMakeFiles/webserver.dir/util.c.o.d"
+  "/mnt/c/Users/dymas/Desktop/Uni/RN_VS/RN-Praxis/Praxis 2 Norm/praxis2/webserver.c" "CMakeFiles/webserver.dir/webserver.c.o" "gcc" "CMakeFiles/webserver.dir/webserver.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
